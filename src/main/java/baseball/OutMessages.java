d6d1650509fb102b41cb09313905a4ff424410cb
taek2222 <oot309@daum.net>
2024-08-14 21:00:56 +0900

    feat: 출력 문장 상수 정의 | @taek2222

---
+++ src/main/java/baseball/OutMessages.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public abstract class OutMessages {
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String GAME_END = "개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String NUMBER_READ = "숫자를 입력해주세요 : ";
+
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+}

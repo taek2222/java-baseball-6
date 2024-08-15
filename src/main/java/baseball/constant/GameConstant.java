e9299da42e29f023c3b5054374b6f3d1b9421301
taek2222 <oot309@daum.net>
2024-08-16 01:27:16 +0900

    feat: 게임 관련 옵션, 입출력 메시지 상수 정의 | @taek2222

---
+++ src/main/java/baseball/constant/GameConstant.java
@@ -0,0 +1,7 @@
+package baseball.constant;
+
+public abstract class GameConstant {
+    public static final int BALL_MAX = 3;
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
+}

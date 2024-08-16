d6b57f4b46a21d3796b1f1ae8f1b7f10b43026e4
taek2222 <oot309@daum.net>
2024-08-16 16:19:01 +0900

    fix: 문제에 제시된 라이브러리 외에 다른 라이브러리 사용으로 오류 발생 해결 | @taek2222

--- src/main/java/baseball/model/ComputerBall.java
+++ src/main/java/baseball/model/ComputerBall.java
@@ -2,13 +2,21 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerBall {
     public List<Integer> computerBalls;
 
     public void setComputerBalls() {
-        computerBalls = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        computerBalls = new ArrayList<>();
+
+        while (computerBalls.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerBalls.contains(randomNumber)) {
+                computerBalls.add(randomNumber);
+            }
+        }
     }
 
     public int getBall(int i) {

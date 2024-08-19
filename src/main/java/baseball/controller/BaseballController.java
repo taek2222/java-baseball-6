6eba29ac3bfc199fd05da60fc09a097d273c606f
taek2222 <oot309@daum.net>
2024-08-19 20:13:20 +0900

    feat: 메서드 분리를 위한 클래스 추가로 의존성 주입 | @taek2222

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -4,22 +4,26 @@ import baseball.exception.Exception;
 import baseball.model.ComputerBall;
 import baseball.model.UserBall;
 import baseball.service.BallService;
+import baseball.service.BallSetService;
 
-import static baseball.constant.ViewConstant.*;
-import static baseball.view.OutputView.outputMessage;
+import static baseball.constant.ViewConstant.GAME_START;
+import static baseball.view.Output.printlnMessage;
 
 public class BaseballController {
     ComputerBall computerBall = new ComputerBall();
     UserBall userBall = new UserBall();
     Exception exception = new Exception();
-    BallService service = new BallService(computerBall, userBall, exception);
+
+    BallSetService ballSetService = new BallSetService(computerBall, userBall, exception);
+    BallService ballService = new BallService(ballSetService, computerBall, userBall, exception);
 
     public void run() {
-        outputMessage(GAME_START);
+        printlnMessage(GAME_START.getMessage());
 
         while (true) {
-            if(service.baseballGame())
-                break;
+            if(ballService.baseballGame())
+                continue;
+            break;
         }
     }
 }

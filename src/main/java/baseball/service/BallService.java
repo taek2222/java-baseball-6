34e63db78dd53a28c4e61ac459572a6a537e8bd0
taek2222 <oot309@daum.net>
2024-08-19 20:15:12 +0900

    refactor: 역할 클래스 분할과 기능 별로 메서드 분할 | @taek2222

--- src/main/java/baseball/service/BallService.java
+++ src/main/java/baseball/service/BallService.java
@@ -6,69 +6,92 @@ import baseball.model.UserBall;
 
 import static baseball.constant.GameConstant.*;
 import static baseball.constant.ViewConstant.*;
-import static baseball.view.InputView.inputMessage;
-import static baseball.view.OutputView.outputMessage;
+import static baseball.view.Input.readInput;
+import static baseball.view.Output.printlnMessage;
+import static baseball.view.Output.printMessage;
 
 public class BallService {
+    private final BallSetService ballSetService;
     private final ComputerBall computerBall;
     private final UserBall userBall;
     private final Exception exception;
 
-    public BallService(ComputerBall computerBall, UserBall userBall, Exception exception) {
+    public BallService(BallSetService ballSetService, ComputerBall computerBall, UserBall userBall, Exception exception) {
+        this.ballSetService = ballSetService;
         this.computerBall = computerBall;
         this.userBall = userBall;
         this.exception = exception;
     }
 
     public boolean baseballGame() {
-        computerBall.setComputerBalls();
+        ballSetService.setComputerBall();
 
         while (true) {
-            promptUserBalls();
+            ballSetService.setUserBalls();
 
-            int strike = 0;
-            int ball = 0;
+            int strike = getStrike();
+            int ball = getBall();
 
-            for (int i = 0; i < BALL_MAX; i++)
-                if(computerBall.getBall(i) == userBall.getBall(i))
-                    strike++;
+            printStrikeBall(ball, strike);
 
-            for (int i = 0; i < BALL_MAX; i++)
-                for (int j = 0; j < BALL_MAX; j++) {
-                    if (i == j)
-                        continue;
-                    if (computerBall.getBall(i) == userBall.getBall(j))
-                        ball++;
-                }
-
-            if (ball > 0 && strike > 0) {
-                outputMessage(ball + BALL.getMessage() + strike + STRIKE.getMessage());
-                continue;
-            }
-
-            if (ball > 0) {
-                outputMessage(ball + BALL.getMessage());
-                continue;
-            }
-
-            if (strike > 0) {
-                outputMessage(strike + STRIKE.getMessage());
-                if (strike == BALL_MAX)
-                    break;
-                continue;
-            }
-
-            outputMessage(NOTHING.getMessage());
+            if (isMaxStrikeBoolean(strike))
+                break;
         }
 
-        outputMessage(GAME_END.getMessage());
-        outputMessage(RESTART_EXIT.getMessage());
-        return !exception.endInputCheck(inputMessage()).equals(RESTART);
+        printGameEnd();
+        return checkRestartExit();
     }
 
-    private void promptUserBalls() {
-        String balls = inputMessage(NUMBER_INPUT.getMessage());
-        exception.inputCheck(balls);
-        userBall.setUserBall(balls);
+    private void printGameEnd() {
+        printlnMessage(BALL_MAX + GAME_END.getMessage());
+        printlnMessage(RESTART_EXIT.getMessage());
+    }
+
+    private boolean checkRestartExit() {
+        String endInput = readInput();
+        exception.endInputCheck(endInput);
+
+        return endInput.equals(RESTART);
+    }
+
+    private boolean isMaxStrikeBoolean(int strike) {
+        return strike == BALL_MAX;
+    }
+
+    private void printStrikeBall(int ball, int strike) {
+        if (ball > 0)
+            printMessage(ball + BALL.getMessage());
+
+        if (strike > 0)
+            printMessage(strike + STRIKE.getMessage());
+
+        if(ball == 0 && strike == 0)
+            printMessage(NOTHING.getMessage());
+
+        printlnMessage("");
+    }
+
+    private int getBall() {
+        int ball = 0;
+
+        for (int i = 0; i < BALL_MAX; i++)
+            for (int j = 0; j < BALL_MAX; j++) {
+                if (i == j)
+                    continue;
+                if (computerBall.getBall(i) == userBall.getBall(j))
+                    ball++;
+            }
+
+        return ball;
+    }
+
+    private int getStrike() {
+        int strike = 0;
+
+        for (int i = 0; i < BALL_MAX; i++)
+            if(computerBall.getBall(i) == userBall.getBall(i))
+                strike++;
+
+        return strike;
     }
 }
